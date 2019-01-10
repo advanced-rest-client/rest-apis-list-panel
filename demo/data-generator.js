26733e45e22735dbc025b6670225f082748d7c6c
Pawel Psztyc <jarrodek@gmail.com>
2019-01-10 00:28:33 -0800

    Updating tests and component logic

--- demo/data-generator.js
+++ demo/data-generator.js
@@ -1,22 +1,17 @@
-/* global chance, PouchDB */
+/* global chance */
 const DataGenerator = {};
 
 DataGenerator.createApiObject = function() {
-  const title = chance.sentence({words: 2});
   const version = 'v' + chance.integer({min: 1, max: 5});
-  const baseUri = chance.url();
-  const _id = baseUri.toLowerCase();
-
   const project = {
-    _id,
-    title,
-    order: 0,
+    _id: chance.string({length: 12}),
+    title: chance.sentence({words: 2}),
     versions: [version],
-    latest: version
+    latest: version,
+    order: 0
   };
   return project;
 };
-
 DataGenerator.generateApis = function(size) {
   size = size || 5;
   const result = [];
@@ -29,14 +24,16 @@ DataGenerator.generateApiData = function(apisIndex) {
   const result = [];
   apisIndex.forEach((item) => {
     result.push({
-      _id: item._id + '|' + item.version,
-      version: item.version,
+      _id: item._id + '|' + item.latest,
+      data: {},
+      version: item.latest,
       indexId: item._id
     });
   });
   return result;
 };
 DataGenerator.generateData = function(size) {
+  /* global PouchDB */
   const projects = DataGenerator.generateApis(size);
   const data = DataGenerator.generateApiData(projects);
   const indexDb = new PouchDB('api-index');
