fe71226d3ba1034a62f18ba1440baab8e86a0223
Paweł Psztyć <jarrodek@gmail.com>
2019-10-29 11:41:56 -0700

    chore: upgrading dependency list

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -5,7 +5,7 @@ const createBaseConfig = require('./karma.conf.js');
 
 module.exports = (config) => {
   const cnf = slSettings();
-  cnf.sauceLabs.testName = 'request-editor';
+  cnf.sauceLabs.testName = 'rest-apis-list-panel';
   cnf.browsers = [
     'SL_Chrome',
     'SL_Chrome-1',
